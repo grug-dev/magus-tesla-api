2de76fea7b529d91cae81bfd357da6321f975049
Cristian Pena <cristiancamilopena@gmail.com>
2026-08-19 09:25:08 -0500

    Adding Card body css

--- internal/gateway/templates/ui/card_templ.go
+++ internal/gateway/templates/ui/card_templ.go
@@ -55,7 +55,7 @@ func Card(p CardProps) templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 2, "\"><div class=\"card-body\">")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 2, "\"><div class=\"card-body bg-[var(--color-surface-card)] border border-[var(--color-line-card)]\">")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
